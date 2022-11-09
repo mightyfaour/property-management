95cac6915d50080b4781fbb05db5cc0fdeb0ac62
techub <tech4phil@outlook.com>
2022-11-09 17:37:53 +0100

    5

--- src/main/java/com/mystartupcompany/propertymanagement/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/mystartupcompany/propertymanagement/service/impl/PropertyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mystartupcompany.propertymanagement.service.impl;
 
 import com.mystartupcompany.propertymanagement.DTOs.PropertyDTO;
+import com.mystartupcompany.propertymanagement.entity.PropertyEntity;
 import com.mystartupcompany.propertymanagement.repository.PropertyRepository;
 import com.mystartupcompany.propertymanagement.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,16 @@ public class PropertyServiceImpl implements PropertyService {
     private PropertyRepository propertyRepository;
     @Override
     public PropertyDTO saveProperty(PropertyDTO propertyDTO) {
-        propertyRepository.save();
+
+        PropertyEntity pe = new PropertyEntity();
+        pe.setTitle(propertyDTO.getTitle());
+        pe.setOwnersAddress(propertyDTO.getOwnersAddress());
+        pe.setOwnersEmail(propertyDTO.getOwnersEmail());
+        pe.setOwnersFirstName(propertyDTO.getOwnersFirstName());
+        pe.setOwnersLastName(propertyDTO.getOwnersLastName());
+        pe.setPrise(propertyDTO.getPrise());
+        propertyRepository.save(pe);
+
         return null;
     }
 }
